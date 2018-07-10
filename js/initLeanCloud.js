745e5ff2f6b01d47520cd3a3a20c67fa0e2a4abd
Murphys-uestc <1363230877@qq.com>
2018-07-11 01:22:37 +0800

    edit website

--- js/initLeanCloud.js
+++ js/initLeanCloud.js
@@ -1,7 +1,7 @@
 // LeanCloud - 初始化 - 将这里的 APP_ID 和 APP_KEY 替换成自己的应用数据
 // https://leancloud.cn/docs/sdk_setup-js.html#初始化
-var APP_ID = 'aNzdPbcgVO2PH1T85wY8fwfI-gzGzoHsz';
-var APP_KEY = 'kYDmRNhx31v3CJ3OogeHxnfr';
+var APP_ID = 'CxthCLKICQWi3U42j78teLUq-gzGzoHsz';
+var APP_KEY = 'ieYMNOfjtHKYEWohEChuCTTP';
 // 存储服务
 AV.init({
     appId: APP_ID,
